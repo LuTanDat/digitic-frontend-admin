9b55fc2e003f6578b2bfc08042d2963171fe2d48
LuTanDat <ludathoc@gmail.com>
2023-10-01 09:03:26 +0700

    updates_04_57m46s

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,40 +2,147 @@ import React, { useState } from 'react';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  // UploadOutlined,
+  // UserOutlined,
+  // VideoCameraOutlined,
 } from '@ant-design/icons';
+
+import {
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { RiCouponLine } from "react-icons/ri";
+import { ImBlog } from "react-icons/im";
+import { IoIosNotifications } from "react-icons/io";
+import { FaClipboardList, FaBloggerB } from "react-icons/fa";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
+
 import { Layout, Menu, Button, theme } from 'antd';
+import { useNavigate } from 'react-router-dom';
 const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const navigate = useNavigate();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
+        <div className="logo">
+          <h2 className='text-white text-center fs-5 py-3 mb-0'>
+            <span className='sm-logo'>Smart</span>
+            <span className='lg-logo'>SmartTech</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['']}
+          onClick={({ key }) => {
+            if (key == "signout") {
+
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
+              key: '',
+              icon: <AiOutlineDashboard className='fs-4' />,
+              label: 'Dashboard',
             },
             {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              key: 'customer',
+              icon: <AiOutlineUser className='fs-4' />,
+              label: 'Customer',
             },
             {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
+              key: 'catalog',
+              icon: <AiOutlineShoppingCart className='fs-4' />,
+              label: 'Catalog',
+              children: [
+                {
+                  key: 'product',
+                  icon: <AiOutlineUser className='fs-4' />,
+                  label: 'Add Product',
+                },
+                {
+                  key: 'product-list',
+                  icon: <AiOutlineUser className='fs-4' />,
+                  label: 'Product List',
+                },
+                {
+                  key: 'category',
+                  icon: <SiBrandfolder className='fs-4' />,
+                  label: 'Brand',
+                },
+                {
+                  key: 'list-category',
+                  icon: <SiBrandfolder className='fs-4' />,
+                  label: 'Brand List',
+                },
+                {
+                  key: 'category',
+                  icon: <BiCategoryAlt className='fs-4' />,
+                  label: 'Category',
+                },
+                {
+                  key: 'list-category',
+                  icon: <BiCategoryAlt className='fs-4' />,
+                  label: 'Category List',
+                },
+                {
+                  key: 'color',
+                  icon: <AiOutlineBgColors className='fs-4' />,
+                  label: 'Color',
+                },
+                {
+                  key: 'list-color',
+                  icon: <AiOutlineBgColors className='fs-4' />,
+                  label: 'Color List',
+                },
+              ]
+            },
+            {
+              key: 'orders',
+              icon: <FaClipboardList className='fs-4' />,
+              label: 'Orders',
+            },
+            {
+              key: 'blogs',
+              icon: <FaBloggerB className='fs-4' />,
+              label: 'Blogs',
+              children: [
+                {
+                  key: 'blog',
+                  icon: <ImBlog className='fs-4' />,
+                  label: 'Add Blog',
+                },
+                {
+                  key: 'blog-list',
+                  icon: <FaBloggerB className='fs-4' />,
+                  label: 'Blog List',
+                },
+                {
+                  key: 'blog-category',
+                  icon: <ImBlog className='fs-4' />,
+                  label: 'Add Blog Category',
+                },
+                {
+                  key: 'blog-category-list',
+                  icon: <FaBloggerB className='fs-4' />,
+                  label: 'Blog Category List',
+                },
+              ]
+            },
+            {
+              key: 'enquiries',
+              icon: <FaClipboardList className='fs-4' />,
+              label: 'Enquiries',
             },
           ]}
         />
--- src/pages/Resetpassword.js
+++ src/pages/Resetpassword.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import CustomInput from "../components/CustomInput";
 
 const Resetpassword = () => {
   return (
-    <div>
-
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+        <h3 className="text-center title">Reset Password</h3>
+        <p className="text-center">Please Enter your new password.</p>
+        <form action="">
+          <CustomInput
+            type="password"
+            label="Password"
+            id="pass"
+          />
+          <CustomInput
+            type="password"
+            label="Confirm Password"
+            id="confpass"
+          />
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Reset Password
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
