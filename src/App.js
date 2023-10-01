20a452741a6e4c0c08665e3d0a1bfdb3baea40f3
LuTanDat <ludathoc@gmail.com>
2023-10-02 06:25:48 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Resetpassword from "./pages/Resetpassword";
 import Forgotpassword from "./pages/Forgotpassword";
-import MainLayout from "./components/MainLayout";
 import Enquiries from './pages/Enquiries';
 import Bloglist from './pages/Bloglist';
 import Blogcatlist from './pages/Blogcatlist';
+import Orders from './pages/Orders';
+import Customer from './pages/Customer';
+import Colorlist from './pages/Colorlist';
+import Categorylist from './pages/Categorylist';
+import Brandlist from './pages/Brandlist';
+import Productlist from './pages/Productlist';
 
 function App() {
   return (
@@ -21,6 +28,12 @@ function App() {
           <Route path='enquiries' element={<Enquiries />} />
           <Route path='blog-list' element={<Bloglist />} />
           <Route path='blog-category-list' element={<Blogcatlist />} />
+          <Route path='orders' element={<Orders />} />
+          <Route path='customer' element={<Customer />} />
+          <Route path='list-color' element={<Colorlist />} />
+          <Route path='list-category' element={<Categorylist />} />
+          <Route path='list-brand' element={<Brandlist />} />
+          <Route path='list-product' element={<Productlist />} />
         </Route>
       </Routes>
     </Router>
