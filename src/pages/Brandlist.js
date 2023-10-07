84777c6adc3ecbd4f6995e67bc69002a4b768de3
LuTanDat <ludathoc@gmail.com>
2023-10-07 10:31:30 +0700

    updates_12

--- src/pages/Brandlist.js
+++ src/pages/Brandlist.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from "antd";
 import { useDispatch, useSelector } from 'react-redux';
-import { getBrands, resetState } from '../features/brand/brandSlice';
+import { deleteABrand, getBrands, resetState } from '../features/brand/brandSlice';
 import { Link } from 'react-router-dom';
 import { BiEdit } from 'react-icons/bi';
 import { AiFillDelete } from 'react-icons/ai';
+import CustomModal from '../components/CustomModal';
 const columns = [
   {
     title: "SNo",
@@ -22,31 +23,51 @@ const columns = [
 ];
 
 const Brandlist = () => {
+  const [open, setOpen] = useState(false);
+  const [brandId, setBrandId] = useState("");
+  const showModal = (e) => {
+    setOpen(true);
+    setBrandId(e);
+  };
+
+  const hideModal = () => {
+    setOpen(false);
+  };
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(resetState());
     dispatch(getBrands());
   }, [])
 
-  const brandstate = useSelector((state) => state.brand.brands);
+  const brandState = useSelector((state) => state.brand.brands);
   const data1 = [];
-  for (let i = 0; i < brandstate.length; i++) {
+  for (let i = 0; i < brandState.length; i++) {
     data1.push({
       key: i + 1,
-      name: brandstate[i].title,
+      name: brandState[i].title,
       action: (
         <>
-          <Link to={`/admin/brand/${brandstate[i]._id}`}
+          <Link to={`/admin/brand/${brandState[i]._id}`}
             className='fs-3 text-danger'>
             <BiEdit />
           </Link>
-          <Link to='list-brand'
-            className='ms-3 fs-3 text-danger'>
+          <button
+            className='ms-3 fs-3 text-danger bg-transparent border-0'
+            onClick={() => showModal(brandState[i]._id)}
+          >
             <AiFillDelete />
-          </Link>
+          </button>
         </>
       )
     });
+  }
+  const deleteBrand = (e) => {
+    dispatch(deleteABrand(e));
+    setOpen(false);
+    setTimeout(() => {
+      dispatch(getBrands());
+    }, 100);
   }
   return (
     <div>
@@ -54,6 +75,12 @@ const Brandlist = () => {
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        hideModal={hideModal}
+        open={open}
+        performAction={() => deleteBrand(brandId)}
+        title="Are you sure you want to delete this brand ?"
+      />
     </div>
   )
 }
