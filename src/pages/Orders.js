3cea6bbae8a1a7ae1c7de94c8e58f285d2c49465
LuTanDat <ludathoc@gmail.com>
2023-11-03 11:29:13 +0700

    updates-print-order

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -102,7 +102,7 @@ const Orders = () => {
         <>
           <button
             className='me-3 fs-3 text-danger bg-transparent border-0'
-            onClick={() => { }}
+            onClick={() => printOrder(orderState[i])}
           >
             <AiFillPrinter />
           </button>
@@ -130,6 +130,92 @@ const Orders = () => {
       dispatch(getOrders(config3));
     }, 100);
   }
+
+  /* Print */
+  const printOrder = (order) => {
+    const orderDetails = getOrderDetails(order); // Tạo chuỗi HTML cho đơn hàng (định dạng in)
+    const popupWin = window.open('', '_blank', 'width=600,height=600');
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>Đơn hàng</title>
+        </head>
+        <body>
+          ${orderDetails}
+        </body>
+      </html>
+    `);
+    popupWin.document.close();
+    popupWin.print();
+    popupWin.onafterprint = () => popupWin.close();
+  };
+  const getOrderDetails = (order) => {
+    // Tạo mảng chuỗi HTML cho các hàng trong bảng
+    const orderItemsHtml = order?.orderItems?.map((item) => `
+          <tr key="${item._id}">
+            <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">
+              ${item.product?.title}
+            </td>
+            <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">
+              ${(item.priceAfterDiscount).toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
+            </td>
+            <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">
+              ${item.quantity}
+            </td>
+            <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">
+              ${(item.priceAfterDiscount * item.quantity).toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
+            </td>
+          </tr>
+        `);
+
+    // Kết hợp các chuỗi HTML để tạo thành chuỗi HTML chính
+    const orderItemsHtmlString = orderItemsHtml.join('');
+
+    // Tạo chuỗi HTML dựa trên thông tin đơn hàng
+    const orderHtml = `
+      <div style="max-width: 680px; margin: 0 auto; padding: 15px; border: 1px solid #ccc;">
+        <div style="text-align: center; margin-bottom: 20px;">
+            <h1>SMARTDEVI HOMES</h1>
+        </div>
+        <div>
+          <i>Địa chỉ shop: Đường 3/2, phường Xuân khánh, quận Ninh Kiều, tp.Cần Thơ</i> <br />
+          <i>Phone: 0919590861</i> <br />
+          <i>Email: ludathoc@gmail.com</i>
+        </div>
+        <div style="display: flex; justify-content: space-between; margin-bottom: 20px;">
+            <div style="flex: 1;">
+                <h4>Thông tin khách hàng:</h4>
+                <p>Tên: ${order?.shippingInfo?.lastName} ${order?.shippingInfo?.firstName}</p>
+                <p>Địa chỉ: ${order?.shippingInfo?.address}</p>
+                <p>Email: ${order?.user?.email}</p>
+            </div>
+            <div style="flex: 1;">
+                <h4>Thông tin hóa đơn:</h4>
+                <p>Số hóa đơn: ${order?._id}</p>
+                <p>Thời gian đặt hàng:  ${new Date(order?.createdAt).toLocaleString()}</p>
+                <p>Phương thức thanh toán:  ${order?.paymentMethod}</p>
+            </div>
+        </div>
+        <table style=" border-collapse: collapse; width: 100%;">
+            <tr>
+                <th style="border: 1px solid #ccc; padding: 8px; text-align: center;">Sản phẩm</th>
+                <th style="border: 1px solid #ccc; padding: 8px; text-align: center;">Giá</th>
+                <th style="border: 1px solid #ccc; padding: 8px; text-align: center;">Số lượng</th>
+                <th style="border: 1px solid #ccc; padding: 8px; text-align: center;">Thành tiền</th>
+            </tr>
+            ${orderItemsHtmlString}
+        </table>
+        <div style="margin-top: 20px; text-align: right;">
+            <b>Tổng cộng: 425,000 VND</b>
+        </div>
+        <div>Cảm ơn quý khách đã tin tưởng và ủng hộ. Xin cảm ơn!</div>
+    </div>
+    `;
+    return orderHtml;
+  };
+
+
   return (
     <div>
       <h3 className='mb-3 title'>Đơn hàng</h3>
